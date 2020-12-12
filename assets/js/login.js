324bc9f2d87a2da32d589b9b1240fb6d99065880
zhn617 <1229438345@qq.com>
2020-12-12 20:09:29 +0800

    first

---
+++ assets/js/login.js
@@ -0,0 +1,63 @@
+$(function(){
+$('.first-2 a').on('click',function(){
+    $('.first-2').hide()
+    $('.first-3').show() 
+})
+
+
+$('.first-3 a').on('click',function(){
+    $('.first-3').hide()
+    $('.first-2').show() 
+})
+
+let  form = layui.form;
+let  layer = layui.layer;
+
+form.verify({
+    pass: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'] ,
+    repass:function(val){
+    if(val !==$('.passInt').val()){
+    return '密码不一致'
+         }
+    }
+})
+
+$('.registered').on('submit',function(e){
+    e.preventDefault()
+    let data = $(this).serialize()
+    console.log(data)
+  $.ajax({
+      url:"http://ajax.frontend.itheima.net/api/reguser",
+      type:'POST',
+      data,
+      success:function(res){
+         if(res.status !==0){
+            return layer.msg(res.message);
+         }
+         layer.msg('注册成功')
+         $('.registered')[0].reset();
+         $('.first-3 a').click()
+      }
+    })
+})
+
+$('.login').on('submit',function(e){
+    e.preventDefault()
+    let data = $(this).serialize()
+  $.ajax({
+      url:'http://ajax.frontend.itheima.net/api/login',
+      type:"POST",
+      data,
+      success:function(res){
+        if(res.message !==0){
+            return layer.msg(res.message);
+     }
+     layer.msg('登入成功', {
+        time: 2000 //2秒关闭（如果不配置，默认是3秒）
+      }, function(){
+        location.href = "/home/index.html";
+      }); 
+    }
+  })               
+ })
+})
